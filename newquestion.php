2520953d388ddf3923262deebd5b39449ea98ed3
aureliehennebel <aurelie.hennebel@etu.unilim.fr>
2017-02-14 12:20:03 +0100

    dernier reglages

--- newquestion.php
+++ newquestion.php
@@ -4,9 +4,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Quizzy</title>
+        <link rel="stylesheet" href="main.css" />
     </head>
     <body>
-        <h1>Merci !</h1>        
+        <div class="content">        
         <?php
         //A faire enregistrer les questions dans la BDD        
         $collection=$db->Questions;        
@@ -15,24 +16,25 @@
         $proposition = $_POST['proposition'];
         $rang = $_POST['rang'];
         $reponse = $_POST['reponse'];
+        $correcte = false;       
         
-        for($i=0;$i<count($proposition);$i++){
-            if($proposition[$i]==$reponse){
-                $q=array('question'=>$sujet, 'propositions'=>$proposition, 'rang'=>$rang, 'reponse'=>$reponse);
-                $collection->insert($q);
-                echo "<p>Votre question a bien été soumise et pourra être validée par les autres utilisateurs !</p>";
-            }
-            else
-            {
-                echo "<p>Vous devez écrire une réponse correcte</p>";
-                break;                
-            }
+       
+         for($i=0;$i<count($proposition);$i++){
+            if($proposition[$i] == $reponse){                
+                $correcte = true;
+            }           
         }       
-        
+        if($correcte = true){
+            $q=array('question'=>$sujet, 'propositions'=>$proposition, 'rang'=>$rang, 'reponse'=>$reponse, 'status'=>'en_attente');
+            $collection->insert($q);
+            echo "<p>Votre question a bien été soumise et pourra être validée par les autres utilisateurs !</p>";
+        }else{
+            echo '<p>Votre réponse ne correspond pas aux proposition réessayez !</p>';
+        }
         
         
         ?>
-        
-        <a href="menu.php">retour au menu</a>
+        </div>
+        <a href="menu.php">retour au menu</a><a href="valid.php">retour aux questions</a>
     </body>
 </html>
--- submit.php
+++ submit.php
@@ -10,21 +10,21 @@
         <div class="content">
         <form id="submit" method="post" action="newquestion.php">
             <label>Votre question<input type="text" name="question"/></label></br>
-            <label>1er choix de réponse : <input type="text" name="proposition[]"/></label></br>
-            <label>2 ème choix de réponse : <input type="text" name="proposition[]"/></label></br>
-            <label>3 ème choix de réponse : <input type="text" name="proposition[]"/></label></br>
+            <label>1er choix de réponse : <input type="text" name="proposition[]"></label></br>
+            <label>2 ème choix de réponse : <input type="text" name="proposition[]"></label></br>
+            <label>3 ème choix de réponse : <input type="text" name="proposition[]"></label></br>
             <label>Bonne réponse : <input type="text" name="reponse"/></label></br>
             <p>Difficulté de la question :</p>
-            <label class="radio"><input type="radio" name="rang" value=1/>1</label>
-            <label class="radio"><input type="radio" name="rang" value=2/>2</label>
-            <label class="radio"><input type="radio" name="rang" value=3/>3</label>
-            <label class="radio"><input type="radio" name="rang" value=4/>4</label>
-            <label class="radio"><input type="radio" name="rang" value=5/>5</label>
-            <label class="radio"><input type="radio" name="rang" value=6/>6</label>
-            <label class="radio"><input type="radio" name="rang" value=7/>7</label>
-            <label class="radio"><input type="radio" name="rang" value=8/>8</label>
-            <label class="radio"><input type="radio" name="rang" value=9/>9</label>
-            <label class="radio"><input type="radio" name="rang" value=10/>10</label></br>
+            <label class="radio"><input type="radio" name="rang" value=1>1</label>
+            <label class="radio"><input type="radio" name="rang" value=2>2</label>
+            <label class="radio"><input type="radio" name="rang" value=3>3</label>
+            <label class="radio"><input type="radio" name="rang" value=4>4</label>
+            <label class="radio"><input type="radio" name="rang" value=5>5</label>
+            <label class="radio"><input type="radio" name="rang" value=6>6</label>
+            <label class="radio"><input type="radio" name="rang" value=7>7</label>
+            <label class="radio"><input type="radio" name="rang" value=8>8</label>
+            <label class="radio"><input type="radio" name="rang" value=9>9</label>
+            <label class="radio"><input type="radio" name="rang" value=10>10</label></br>
             <input type='submit' value="Soumettre la question"/>
             <a href="menu.php" class="quitter">Quitter</a>
         </form>
